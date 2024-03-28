1dd0b4ec90cbd57a0c2306b736240887219359c7
Rambali Kumar <rambali@izooto.com>
2024-03-28 11:40:26 +0530

    initial commit

---
+++ WidgetSwiftUi/WidgetSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSwiftUiApp.swift
+//  WidgetSwiftUi
+//
+//  Created by Rambali Kumar on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
